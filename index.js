14287656dc48632f4e7b20c507d6113f5ad35a58
Alice Hepburn <aliceokhepburn@gmail.com>
2023-06-06 15:51:25 -0400

    phase-0-javascript events listening lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-function addingEventListener() {
+const input = document.getElementById('button')
+function clickAlert() {
+    alert("I was clicked!")
 }
+
+function addingEventListener() {
+input.addEventListener('click', clickAlert)
+}
+
+addingEventListener()
+
+//We know that the code works in the console and passes the test, but we should also check our changes to index.js in the browser. Because you've added the addEventListener() function inside the addingEventListener() function, recall that you will need to call the outer function in index.js to execute addEventListener() and activate the event listener. Be sure to refresh the page to load the new code in index.js.
+
+
